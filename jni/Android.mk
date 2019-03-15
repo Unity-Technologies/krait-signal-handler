1419745eb9d40b7a9619d68410cdec3ba371e3e8
Ignas Ziberkas <ignas@unity3d.com>
2019-03-15 11:13:50 +0200

    Switch to Thumb instruction set.

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,4 +6,8 @@ LOCAL_MODULE    := krait-signal-handler
 LOCAL_SRC_FILES := krait_signal_handler.c
 LOCAL_CFLAGS    := -Wall -Werror
 
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+    LOCAL_CFLAGS += -mthumb
+endif
+
 include $(BUILD_STATIC_LIBRARY)
